cdfd464f836f23bf13a00a3a84172ba65321fb3b
Raphael <paiva.raphaelt@gmail.com>
2020-08-27 01:49:42 +0200

    structure server.ts

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,2 +1,21 @@
-export const addition = (a: number, b: number): number => a + b; 
-console.log('hello')
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import Connection from "./Provider/Connection";
+import { log } from './Provider/logDB';
+dotenv.config();
+
+const connectToDB: Connection = new Connection(log);
+connectToDB.atMongoDB();
+
+const app: express.Application = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(process.env.PORT, () => console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`));
