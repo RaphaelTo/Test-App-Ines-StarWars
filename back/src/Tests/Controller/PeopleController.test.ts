037c251be915a8358799428fc1c3609a30f346ed
Raphael <paiva.raphaelt@gmail.com>
2020-08-27 16:56:52 +0200

    create test "create a people"

--- back/src/Tests/Controller/PeopleController.test.ts
+++ back/src/Tests/Controller/PeopleController.test.ts
@@ -1,21 +1,26 @@
-import mongoose from 'mongoose';
-import {mocked} from "ts-jest/utils";
+import { mocked } from "ts-jest/dist/util/testing";
 
+import People from "../../Models/People";
 import PeopleController from '../../Controllers/PeopleController';
 import { peopleType } from "../../Types/peopleType";
 
+jest.mock('../../Models/People');
 
-jest.mock('mongoose');
-const mockedMongoose = mocked(mongoose, true);
+const mockedPeople = mocked(People, true);
 
 describe('Controller PeopleController', () => {
+
+    beforeEach(() => {
+        mockedPeople.mockReset();
+    });
+
     test('class exist', () => {
         const peopleController: PeopleController = new PeopleController();
         expect(peopleController).not.toBeNull();
     });
 
     test('method createPeople exist', async () => {
-        const mockValueToCreateUser: peopleType = {
+        const mockValueToCreatePeople: peopleType = {
             birth_year: "",
             created: "",
             edited: "",
@@ -37,9 +42,43 @@ describe('Controller PeopleController', () => {
 
         expect.assertions(1);
         // @ts-ignore
-        mockedMongoose.Model.find.mockResolvedValue(returnMock);
-        const peopleController: PeopleController = await PeopleController.createPeople(mockValueToCreateUser);
+        mockedPeople.create.mockResolvedValue(returnMock);
+        const peopleController: PeopleController = await PeopleController.createPeople(mockValueToCreatePeople);
 
         expect(peopleController).not.toBeNull();
-    })
+    });
+
+    test('method createPeople create a people', async () => {
+        const mockValueToCreatePeople: peopleType = {
+            birth_year: "2018",
+            created: "today",
+            edited: "yesterday",
+            eye_color: "blue",
+            gender: "male",
+            hair_color: "short",
+            height: "58kg",
+            homeworld: "earth",
+            mass: "idk",
+            name: "Raphael",
+            skin_color: "blue",
+            url: "portfolio-raphael-torres.fr",
+            films:  ["a","a"],
+            species: ["a","a"],
+            starships: ["a","a"],
+            vehicles: ["a","a"],
+        };
+        const returnMock: {_id: string, name: string} = {
+            _id: 'a',
+            name: 'Raphael'
+        };
+
+        expect.assertions(3);
+        // @ts-ignore
+        mockedPeople.create.mockResolvedValue(returnMock);
+        const peopleController: PeopleController = await PeopleController.createPeople(mockValueToCreatePeople);
+
+        expect(mockedPeople.create).toHaveBeenCalled();
+        expect(peopleController).toEqual(peopleController);
+        expect(typeof peopleController).toBe('object');
+    });
 });
