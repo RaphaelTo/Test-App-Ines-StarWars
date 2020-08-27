73ef60218060c80703b583f37c253ec52b441a36
Raphael <paiva.raphaelt@gmail.com>
2020-08-27 18:22:57 +0200

    can be undefined

--- back/src/Interfaces/IPeople.ts
+++ back/src/Interfaces/IPeople.ts
@@ -1,20 +1,20 @@
 import * as mongoose from "mongoose";
 
 export interface IPeople extends mongoose.Document {
-    name: string;
-    birth_year: string;
-    eye_color: string;
-    gender: string;
-    hair_color: string;
-    height: string;
-    mass: string;
-    skin_color: string;
-    homeworld: string;
-    films: Array<string>;
-    species: Array<string>;
-    starships: Array<string>;
-    vehicles: Array<string>;
-    url: string;
-    created: string;
-    edited: string;
+    name?: string;
+    birth_year?: string;
+    eye_color?: string;
+    gender?: string;
+    hair_color?: string;
+    height?: string;
+    mass?: string;
+    skin_color?: string;
+    homeworld?: string;
+    films?: Array<string>;
+    species?: Array<string>;
+    starships?: Array<string>;
+    vehicles?: Array<string>;
+    url?: string;
+    created?: string;
+    edited?: string;
 }
