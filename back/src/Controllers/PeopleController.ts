4996cfa047786e2baab868106716a6887c229295
Raphael <paiva.raphaelt@gmail.com>
2020-08-27 03:34:23 +0200

    create methode createPeople

--- back/src/Controllers/PeopleController.ts
+++ back/src/Controllers/PeopleController.ts
@@ -1,5 +1,10 @@
+import { peopleType } from "../Types/peopleType";
+
 class PeopleController {
 
+    public static async createPeople(people: peopleType): Promise<any> {
+        return {};
+    }
 }
 
 export default PeopleController;
