660eea3918fc242873591403c7f822b0ebabbe07
Raphael <paiva.raphaelt@gmail.com>
2020-08-28 01:24:24 +0200

    add route people

--- back/src/server.ts
+++ back/src/server.ts
@@ -4,9 +4,12 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
 import Connection from "./Provider/Connection";
 import { log } from './Provider/logDB';
-dotenv.config();
+
+import {peopleRoute} from "./Routes/PeopleRoute";
 
 const connectToDB: Connection = new Connection(log);
 connectToDB.atMongoDB();
@@ -17,5 +20,7 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(`${process.env.URL}/people`, peopleRoute);
 
 app.listen(process.env.PORT, () => console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`));
