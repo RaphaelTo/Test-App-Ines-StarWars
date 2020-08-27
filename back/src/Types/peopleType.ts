d9e28c54549c1eb3023413a694ad643881aeb58e
Raphael <paiva.raphaelt@gmail.com>
2020-08-27 18:13:23 +0200

    can be undefined

--- back/src/Types/peopleType.ts
+++ back/src/Types/peopleType.ts
@@ -1,18 +1,18 @@
 export type peopleType = {
-    name: string;
-    birth_year: string;
-    eye_color: string;
-    gender: string;
-    hair_color: string;
-    height: string;
-    mass: string;
-    skin_color: string;
-    homeworld: string;
-    films: Array<string>;
-    species: Array<string>;
-    starships: Array<string>;
-    vehicles: Array<string>;
-    url: string;
-    created: string;
-    edited: string;
+    name?: string;
+    birth_year?: string;
+    eye_color?: string;
+    gender?: string;
+    hair_color?: string;
+    height?: string;
+    mass?: string;
+    skin_color?: string;
+    homeworld?: string;
+    films?: Array<string>;
+    species?: Array<string>;
+    starships?: Array<string>;
+    vehicles?: Array<string>;
+    url?: string;
+    created?: string;
+    edited?: string;
 }
