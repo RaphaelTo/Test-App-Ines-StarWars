ae672d65aeb3a41462f9cdfeaaa488e88cafc22c
Raphael <paiva.raphaelt@gmail.com>
2020-08-27 16:57:19 +0200

    return people created

--- back/src/Controllers/PeopleController.ts
+++ back/src/Controllers/PeopleController.ts
@@ -1,9 +1,13 @@
 import { peopleType } from "../Types/peopleType";
+import People from "../Models/People";
+import { IPeople } from "../Interfaces/IPeople";
+import { successResponse } from "../responseJson";
 
 class PeopleController {
 
     public static async createPeople(people: peopleType): Promise<any> {
-        return {};
+        const createPeople: IPeople = await People.create(people);
+        return successResponse<any>(createPeople);
     }
 }
 
