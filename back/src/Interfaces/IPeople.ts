1a15f351acace1a91ebacd56877f025fc53dcd3d
Raphael <paiva.raphaelt@gmail.com>
2020-08-27 02:42:38 +0200

    add array films, species, starships, vehicles

--- back/src/Interfaces/IPeople.ts
+++ back/src/Interfaces/IPeople.ts
@@ -10,7 +10,10 @@ export interface IPeople extends mongoose.Document {
     mass: string;
     skin_color: string;
     homeworld: string;
-    [index: number]: string;
+    films: Array<string>;
+    species: Array<string>;
+    starships: Array<string>;
+    vehicles: Array<string>;
     url: string;
     created: string;
     edited: string;
