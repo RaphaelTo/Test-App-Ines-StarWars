57c54fa586156535792929aa634f39afcfb4c41d
Raphael <paiva.raphaelt@gmail.com>
2020-08-27 17:39:18 +0200

    add try and catch

--- back/src/Controllers/PeopleController.ts
+++ back/src/Controllers/PeopleController.ts
@@ -1,13 +1,19 @@
 import { peopleType } from "../Types/peopleType";
 import People from "../Models/People";
 import { IPeople } from "../Interfaces/IPeople";
-import { successResponse } from "../responseJson";
+import {errorResponse, successResponse} from "../responseJson";
+import {ResponseSuccessType} from "../Types/ResponseSuccessType";
+import {ResponseErrorType} from "../Types/ResponseErrorType";
 
 class PeopleController {
 
-    public static async createPeople(people: peopleType): Promise<any> {
-        const createPeople: IPeople = await People.create(people);
-        return successResponse<any>(createPeople);
+    public static async createPeople(people: peopleType): Promise<ResponseSuccessType<any> | ResponseErrorType<any>> {
+        try {
+            const createPeople: IPeople = await People.create(people);
+            return successResponse (createPeople);
+        }catch (e) {
+            return errorResponse(e);
+        }
     }
 }
 
