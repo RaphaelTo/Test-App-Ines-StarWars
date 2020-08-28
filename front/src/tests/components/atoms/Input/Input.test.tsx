947a50290d024b3b29c3036bc30ab5016f58d1e1
Raphael <paiva.raphaelt@gmail.com>
2020-08-28 14:42:42 +0200

    test input have an id

--- front/src/tests/components/atoms/Input/Input.test.tsx
+++ front/src/tests/components/atoms/Input/Input.test.tsx
@@ -16,10 +16,24 @@ describe('component input', () => {
         expect(getElement instanceof HTMLInputElement).toBeTruthy();
     });
 
-    test('atom input return type="text by default', () => {
+    test('atom input return type="text" "by default', () => {
         const { getByTestId } = render(<Input />);
         const getElement = getByTestId('input-test');
 
         expect(getElement.type).toBe('text');
-    })
+    });
+
+    test('atom input return type="password"', () => {
+        const { getByTestId } = render(<Input type="password" /> );
+        const getElement = getByTestId('input-test');
+
+        expect(getElement.type).toBe('password');
+    });
+
+    test('atom input have an id', () => {
+        const { getByTestId } = render(<Input type="password" id="label-id"/> );
+        const getElement = getByTestId('input-test');
+
+        expect(getElement.id).toBe('label-id');
+    });
 })
