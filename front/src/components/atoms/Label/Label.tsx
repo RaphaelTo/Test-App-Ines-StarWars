c21f32b7d6d1360b0234a01c00b84c5a08b1929f
Raphael <paiva.raphaelt@gmail.com>
2020-08-28 15:30:53 +0200

    add interface and proptype

--- front/src/components/atoms/Label/Label.tsx
+++ front/src/components/atoms/Label/Label.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 interface IPropsLabel {
-    htmlFor: string,
-    content: string
+    htmlFor?: string,
+    content?: string
 }
 
 const Label: React.FunctionComponent<IPropsLabel> = ({htmlFor, content}) => {
     return <label htmlFor={htmlFor} data-testid="label-id">{content}</label>
+};
+
+Label.propTypes = {
+    htmlFor: PropTypes.string,
+    content: PropTypes.string
+};
+
+Label.defaultProps = {
+    htmlFor: 'valeur par defaut',
+    content: 'valeur par defaut'
 };
 
 export default Label;
