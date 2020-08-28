1cc954e8ebef05b1d4eef3fcadb6affb8a080f97
Raphael <paiva.raphaelt@gmail.com>
2020-08-28 14:34:04 +0200

    add proptype default

--- front/src/components/atoms/Input/Input.tsx
+++ front/src/components/atoms/Input/Input.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 interface IPropsInput {
     type?: string
 }
 
 const Input: React.FunctionComponent<IPropsInput> = ({type}) => (
-    <input type='text' data-testid='input-test'/>
+    <input type={type} data-testid='input-test'/>
 );
+
+
+Input.propTypes = {
+    type: PropTypes.string
+};
+
+Input.defaultProps = {
+    type: 'text'
+};
 
 export default Input;
