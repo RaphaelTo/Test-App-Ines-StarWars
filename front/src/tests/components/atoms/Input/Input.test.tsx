10ff9985bf8400a40e007ab7f3dee3e6475a00ad
Raphael <paiva.raphaelt@gmail.com>
2020-08-28 14:27:18 +0200

    create test atom return an input

--- front/src/tests/components/atoms/Input/Input.test.tsx
+++ front/src/tests/components/atoms/Input/Input.test.tsx
@@ -4,9 +4,15 @@ import { render } from '@testing-library/react';
 import Input from '../../../../components/atoms/Input/Input';
 
 describe('component input', () => {
-    test('renders component input', () => {
+    test('renders atom input', () => {
         const inputComponent = render(<Input />);
         expect(inputComponent).toBeTruthy();
     });
 
+    test('atom input return an input', () => {
+        const { getByTestId } = render(<Input />);
+        const getElement = getByTestId('input-test');
+
+        expect(getElement instanceof HTMLInputElement).toBeTruthy();
+    })
 })
