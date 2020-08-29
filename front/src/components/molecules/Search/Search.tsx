2ae31de86f985320847bd0492151661cb4416f81
Raphael <paiva.raphaelt@gmail.com>
2020-08-29 16:13:10 +0200

    add interface, and proptypes for default value

--- front/src/components/molecules/Search/Search.tsx
+++ front/src/components/molecules/Search/Search.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Label from "../../atoms/Label/Label";
 
-const Search = () => {
-    return <Label />;
+interface IPropsSearch {
+    idLabel?: string,
+    contentLabel?: string
+}
+
+const Search: React.FunctionComponent<IPropsSearch> = ({idLabel, contentLabel}) => {
+    return <Label htmlFor={idLabel} content={contentLabel}/>;
 };
+
+Search.propTypes = {
+    idLabel: PropTypes.string,
+    contentLabel: PropTypes.string
+};
+
+Search.defaultProps = {
+    idLabel: 'valeur par defaut',
+    contentLabel: 'valeur par defaut'
+}
 
 export default Search;
