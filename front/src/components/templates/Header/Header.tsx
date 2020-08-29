447791e3715e2ea269fdae86e19ef6708a63a617
Raphael <paiva.raphaelt@gmail.com>
2020-08-29 19:02:51 +0200

    add interface and props

--- front/src/components/templates/Header/Header.tsx
+++ front/src/components/templates/Header/Header.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Navigation from "../../organisms/Navigation/Navigation";
 
-const Header = () => {
-    return <h1>Hello</h1>
+interface IPropsNavigation {
+    listItems?: Array<string>
+    navigationSearchInputType?: string
+    navigationSearchInputId?: string
+    navigationSearchLabelFor?: string
+    navigationSearchLabelContent?: string
+    [index: string]: any
+};
+
+const Header: React.FunctionComponent<IPropsNavigation> = ({
+    listItems= ['value 1', 'value 2', 'value 3'],
+    navigationSearchInputId= '',
+    navigationSearchInputType= 'text',
+    navigationSearchLabelContent= 'valeur par defaut',
+    navigationSearchLabelFor= 'valeur par defaut',
+    propsOnClickList,
+    ...props
+                                                           }) => {
+    return <Navigation
+        listItems={listItems}
+        searchInputId={navigationSearchInputId}
+        searchInputType={navigationSearchInputType}
+        searchLabelContent={navigationSearchLabelContent}
+        searchLabelFor={navigationSearchLabelFor}
+        propsOnClickList={propsOnClickList}
+        {...props}
+    />
 };
 
 export default Header;
