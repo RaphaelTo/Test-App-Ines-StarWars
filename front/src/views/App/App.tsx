0573fa59cda653d0ded7b69a1447cf29b079e26e
Raphael <paiva.raphaelt@gmail.com>
2020-08-29 23:11:41 +0200

    add css flexbox

--- front/src/views/App/App.tsx
+++ front/src/views/App/App.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import Header from '../../components/templates/Header/Header';
 import './App.css';
 
-
-
 const filterTuples: [string, string, string, string] = ['Sans filtre', 'Nom', 'Sexe', 'Planète'];
 
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
-        <Header
-            listItems={filterTuples}
-            navigationSearchLabelFor='name'
-            navigationSearchLabelContent='Nom: '
-            navigationSearchInputId='name'
-            navigationSearchInputType='text'
-            placeholder='Veuillez renseigner le nom'
-        />
+        <div className="container">
+            <Header
+                listItems={filterTuples}
+                navigationSearchLabelFor='name'
+                navigationSearchLabelContent='Nom: '
+                navigationSearchInputId='name'
+                navigationSearchInputType='text'
+                placeholder='Veuillez renseigner le nom'
+            />
+        </div>
     </div>
   );
 };
