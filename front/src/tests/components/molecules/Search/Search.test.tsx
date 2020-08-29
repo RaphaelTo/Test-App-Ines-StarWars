1f5b275a31a697da2e075f3e616e01aa7e0dc307
Raphael <paiva.raphaelt@gmail.com>
2020-08-29 16:03:07 +0200

    create test to check if atom label is rendered

--- front/src/tests/components/molecules/Search/Search.test.tsx
+++ front/src/tests/components/molecules/Search/Search.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 
 import Search from '../../../../components/molecules/Search/Search';
 
@@ -8,5 +8,13 @@ describe('molecules search', () => {
     test('molecules search exist', () => {
         const getSearch = render(<Search />);
         expect(getSearch).toBeTruthy();
-    })
+    });
+
+    test('molecules search return atom Label', async () => {
+       const { getByTestId, debug } = render(<Search/>);
+       const getLabel = getByTestId('label-id');
+
+       expect(getLabel.htmlFor).toBe('valeur par defaut');
+       expect(getLabel.textContent).toBe('valeur par defaut');
+    });
 })
