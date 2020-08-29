9f4c9e280220d4008880ad07c701a537f50c8e38
Raphael <paiva.raphaelt@gmail.com>
2020-08-29 17:37:44 +0200

    test navigation return atom list

--- front/src/tests/components/organisms/Navigation/Navigation.test.tsx
+++ front/src/tests/components/organisms/Navigation/Navigation.test.tsx
@@ -22,5 +22,28 @@ describe('organisms Navigation', () => {
        expect(getSearchLabel.htmlFor).toBe('valeur par defaut');
        expect(getSearchLabel.textContent).toBe('valeur par defaut');
        expect(getSearchLabel instanceof HTMLLabelElement).toBeTruthy();
-   })
+   });
+
+   test('organism Navigation take props for Search molecule', () => {
+       const { getByTestId } = render(<Navigation searchInputType="password" searchInputId="a" searchLabelFor="a" searchLabelContent="nom"/>);
+       const getSearchInput = getByTestId('input-test');
+       const getSearchLabel = getByTestId('label-id');
+
+       expect(getSearchInput.type).toBe('password');
+       expect(getSearchInput.id).toBe('a');
+       expect(getSearchInput instanceof HTMLInputElement).toBeTruthy();
+       expect(getSearchLabel.htmlFor).toBe('a');
+       expect(getSearchLabel.textContent).toBe('nom');
+       expect(getSearchLabel instanceof HTMLLabelElement).toBeTruthy();
+
+   });
+
+   test('organism Navigation return atom List', () => {
+       const { getAllByTestId } = render(<Navigation />);
+       const getList = getAllByTestId('li-items').map(item => item.textContent);
+
+       expect(getList).toEqual(['Value 1', 'Value 2', 'Value 3']);
+       expect(getAllByTestId('li-items')).toBeTruthy();
+   });
+
 });
