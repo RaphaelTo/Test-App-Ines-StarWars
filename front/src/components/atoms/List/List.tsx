f1592cfe1ec41e8f791f15d43c58cf49462f626f
Raphael <paiva.raphaelt@gmail.com>
2020-08-29 19:35:12 +0200

    delete proptypes add css

--- front/src/components/atoms/List/List.tsx
+++ front/src/components/atoms/List/List.tsx
@@ -1,29 +1,23 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import './List.css';
 
 interface IPropsList {
     items?: Array<string>
     [index: string]: any
 }
 
-const List: React.FunctionComponent<IPropsList> = ({items, propsOnClickList}) => {
+const List: React.FunctionComponent<IPropsList> = ({items = ['Value 1', 'Value 2', 'Value 3'], propsOnClickList}) => {
     return (
       <>
-          <ul>
-              {items?.map((item: string, key: number) => (
-                  <li onClick={propsOnClickList} key={key} data-testid="li-items">{item}</li>
-              ))}
-          </ul>
+          <div className='header-organisms-list'>
+              <ul>
+                  {items?.map((item: string, key: number) => (
+                      <li onClick={propsOnClickList} key={key} data-testid="li-items">{item}</li>
+                  ))}
+              </ul>
+          </div>
       </>
     );
-};
-
-List.propTypes = {
-    items: PropTypes.array
-};
-
-List.defaultProps = {
-    items: ['Value 1', 'Value 2', 'Value 3']
 };
 
 export default List;
