6d52bcd2df07c19d55c061942f93ddbc16bf0ab4
Raphael <paiva.raphaelt@gmail.com>
2020-08-29 17:37:59 +0200

    add interface proptype

--- front/src/components/organisms/Navigation/Navigation.tsx
+++ front/src/components/organisms/Navigation/Navigation.tsx
@@ -1,8 +1,36 @@
 import React from "react";
 import Search from "../../molecules/Search/Search";
+import List from "../../atoms/List/List";
+import PropTypes from "prop-types";
 
-const Navigation = () => {
-    return <Search />
+interface IPropsNavigation {
+    searchInputType?: string
+    searchInputId?: string
+    searchLabelFor?: string
+    searchLabelContent?: string
+}
+
+const Navigation: React.FunctionComponent<IPropsNavigation> = ({searchInputId, searchInputType, searchLabelContent, searchLabelFor}) => {
+    return (
+        <>
+            <Search idInput={searchInputId} typeInput={searchInputType} idLabel={searchLabelFor} contentLabel={searchLabelContent}/>
+            <List />
+        </>
+    )
+};
+
+Navigation.defaultProps = {
+    searchInputId: '',
+    searchInputType: 'text',
+    searchLabelFor: 'valeur par defaut',
+    searchLabelContent: 'valeur par defaut'
+};
+
+Navigation.propTypes = {
+    searchInputId: PropTypes.string,
+    searchInputType: PropTypes.string,
+    searchLabelContent: PropTypes.string,
+    searchLabelFor: PropTypes.string
 };
 
 export default Navigation;
