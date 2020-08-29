c92fa72cbe92900d63d83c120ff76f11ef1fee69
Raphael <paiva.raphaelt@gmail.com>
2020-08-29 16:12:48 +0200

    create test molecules search accept props for atom label

--- front/src/tests/components/molecules/Search/Search.test.tsx
+++ front/src/tests/components/molecules/Search/Search.test.tsx
@@ -10,11 +10,19 @@ describe('molecules search', () => {
         expect(getSearch).toBeTruthy();
     });
 
-    test('molecules search return atom Label', async () => {
+    test('molecules search return atom Label', () => {
        const { getByTestId, debug } = render(<Search/>);
        const getLabel = getByTestId('label-id');
 
        expect(getLabel.htmlFor).toBe('valeur par defaut');
        expect(getLabel.textContent).toBe('valeur par defaut');
+    });
+
+    test('molecules search accept props for atom label', () => {
+       const { getByTestId, debug } = render(<Search idLabel="idL" contentLabel="Nom"/>);
+       const getLabel = getByTestId('label-id');
+
+       expect(getLabel.htmlFor).toBe('idL');
+       expect(getLabel.textContent).toBe('Nom');
     });
 })
