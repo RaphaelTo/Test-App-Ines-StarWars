156c8c70d8479985ed7df274eff92eb9a252f211
Raphael <paiva.raphaelt@gmail.com>
2020-08-30 00:04:04 +0200

    create test atom button get name props for button

--- front/src/tests/components/atoms/Button/Button.test.tsx
+++ front/src/tests/components/atoms/Button/Button.test.tsx
@@ -10,4 +10,13 @@ describe('atom Button', () => {
 
         expect(getButton).not.toBeNull();
     });
+
+    test('atom Button get a name for button', () => {
+        const { getByTestId } = render(<Button name='Load more ...'/>);
+
+        const getButton = getByTestId('button-test');
+
+        expect(getButton instanceof HTMLButtonElement).toBeTruthy();
+        expect(getButton).toHaveTextContent('Load more ...');
+    })
 });
