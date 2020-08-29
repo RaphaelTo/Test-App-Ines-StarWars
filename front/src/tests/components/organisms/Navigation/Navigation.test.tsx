f9963a80ddf014c2030d64a2fd4dc7e6062cb624
Raphael <paiva.raphaelt@gmail.com>
2020-08-29 17:59:25 +0200

    test return search molecule and list atom

--- front/src/tests/components/organisms/Navigation/Navigation.test.tsx
+++ front/src/tests/components/organisms/Navigation/Navigation.test.tsx
@@ -54,4 +54,22 @@ describe('organisms Navigation', () => {
        expect(getList).toEqual(arrayList);
        expect(getAllByTestId('li-items')).toBeTruthy();
    });
+
+   test('organism Navigation return atom List, and molecule Search', () => {
+       const arrayList: Array<string> = ['Nom', 'Prenom', 'Age'];
+       const { getByTestId, getAllByTestId } = render(<Navigation placeholder="Veuillez rentrer votre nom" listItems={arrayList} searchInputType="password" searchInputId="a" searchLabelFor="a" searchLabelContent="nom"/>);
+       const getSearchInput = getByTestId('input-test');
+       const getSearchLabel = getByTestId('label-id');
+       const getList = getAllByTestId('li-items').map(item => item.textContent);
+
+       expect(getSearchInput.placeholder).toBe('Veuillez rentrer votre nom');
+       expect(getList).toEqual(arrayList);
+       expect(getAllByTestId('li-items')).toBeTruthy();
+       expect(getSearchInput.type).toBe('password');
+       expect(getSearchInput.id).toBe('a');
+       expect(getSearchInput instanceof HTMLInputElement).toBeTruthy();
+       expect(getSearchLabel.htmlFor).toBe('a');
+       expect(getSearchLabel.textContent).toBe('nom');
+       expect(getSearchLabel instanceof HTMLLabelElement).toBeTruthy();
+   });
 });
