63f0ad9a4859e35fcca6343da997872f9f1194e7
Raphael <paiva.raphaelt@gmail.com>
2020-08-30 00:54:53 +0200

    add default value at props peoples

--- front/src/components/templates/Content/Content.tsx
+++ front/src/components/templates/Content/Content.tsx
@@ -5,7 +5,12 @@ interface IPropsContent {
     peoples?: Array<object>
 }
 
-const Content: React.FunctionComponent<IPropsContent> = ({peoples}) => {
+const Content: React.FunctionComponent<IPropsContent> = ({
+                                                             peoples= [{
+                                                                 name: 'Default name',
+                                                                 height: 'Default height',
+                                                                 mass: 'Default mass'
+                                                             }] }) => {
     return <People peoples={peoples} />
 };
 
