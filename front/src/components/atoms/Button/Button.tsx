e3de0a508efbcbec3f14664626a13492346e17b4
Raphael <paiva.raphaelt@gmail.com>
2020-08-30 00:25:18 +0200

    add props onClickButton

--- front/src/components/atoms/Button/Button.tsx
+++ front/src/components/atoms/Button/Button.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 interface IPropsButton {
-    name?: string
+    name?: string,
+    onClickButton?: any
 }
 
 
-const Button: React.FunctionComponent<IPropsButton> = ({name = 'Valider'}) => {
-    return <button data-testid="button-test">{name}</button>
+const Button: React.FunctionComponent<IPropsButton> = ({name = 'Valider', onClickButton}) => {
+    return <button onClick={onClickButton} data-testid="button-test">{name}</button>
 };
 
 export default Button;
