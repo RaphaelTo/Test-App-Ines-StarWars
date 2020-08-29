6d444ce777ac648dd9b720658612d7c73cde825d
Raphael <paiva.raphaelt@gmail.com>
2020-08-29 19:02:39 +0200

    templates Header return organism Navigation with value

--- front/src/tests/components/templates/Header/Header.test.tsx
+++ front/src/tests/components/templates/Header/Header.test.tsx
@@ -10,4 +10,22 @@ describe('templates Header', () => {
 
       expect(getHeader).not.toBeNull();
    });
+
+   test('Header render organisms Navigation', () => {
+      const arrayList: Array<string> = ['Nom', 'Prenom', 'Age'];
+      const { getByTestId, getAllByTestId } = render(<Header placeholder="Veuillez rentrer votre nom" listItems={arrayList} navigationSearchInputType="password" navigationSearchInputId="a" navigationSearchLabelFor="a" navigationSearchLabelContent="nom"/>);
+      const getNavigationSearchInput = getByTestId('input-test');
+      const getNavigationSearchLabel = getByTestId('label-id');
+      const getNavigationList = getAllByTestId('li-items').map(item => item.textContent);
+
+      expect(getNavigationSearchInput.placeholder).toBe('Veuillez rentrer votre nom');
+      expect(getNavigationList).toEqual(arrayList);
+      expect(getAllByTestId('li-items')).toBeTruthy();
+      expect(getNavigationSearchInput.type).toBe('password');
+      expect(getNavigationSearchInput.id).toBe('a');
+      expect(getNavigationSearchInput instanceof HTMLInputElement).toBeTruthy();
+      expect(getNavigationSearchLabel.htmlFor).toBe('a');
+      expect(getNavigationSearchLabel.textContent).toBe('nom');
+      expect(getNavigationSearchLabel instanceof HTMLLabelElement).toBeTruthy();
+   });
 });
