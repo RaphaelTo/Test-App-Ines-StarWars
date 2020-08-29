dcd9c66e6f7782c98af1f96bb63d3e21b548cbb7
Raphael <paiva.raphaelt@gmail.com>
2020-08-29 23:48:58 +0200

    create interface, default value, display info

--- front/src/components/atoms/People/People.tsx
+++ front/src/components/atoms/People/People.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
-const People: React.FunctionComponent = () => {
-    return <h1>Hello people</h1>
+interface IPropsPeople {
+    peoples?: Array<object>
+}
+
+const People: React.FunctionComponent<IPropsPeople> = ({peoples = [
+    {
+        name: 'Default name',
+        height: 'Default height',
+        mass: 'Default mass'
+    }
+]}) => {
+    return (
+        <div>
+            {peoples.map((people:any, key: number) => (
+                <div key={key}>
+                    <p data-testid="people-test">{people.name}</p>
+                    <p data-testid="people-test">{people.height}</p>
+                    <p data-testid="people-test">{people.mass}</p>
+                </div>
+            ))}
+        </div>
+    )
 };
 
 export default People
