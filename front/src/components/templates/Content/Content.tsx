c4a4e6f842a95e2f7b55bc806ad8a0d914855a4e
Raphael <paiva.raphaelt@gmail.com>
2020-08-30 03:00:38 +0200

    indentation

--- front/src/components/templates/Content/Content.tsx
+++ front/src/components/templates/Content/Content.tsx
@@ -6,23 +6,24 @@ import Button from "../../atoms/Button/Button";
 
 interface IPropsContent {
     peoples?: Array<object>
-    nameButton ?: string,
-    onClickButton ?: any
+    nameButton?: string,
+    onClickButton?: any
 }
 
 const Content: React.FunctionComponent<IPropsContent> = ({
-                                                             peoples= [{
+                                                             peoples = [{
                                                                  name: 'Default name',
                                                                  height: 'Default height',
                                                                  mass: 'Default mass'
                                                              }],
-                                                             nameButton= 'Valider',
-                                                         onClickButton}) => {
+                                                             nameButton = 'Valider',
+                                                             onClickButton
+                                                         }) => {
     return (
         <>
-            <People peoples={peoples} />
+            <People peoples={peoples}/>
             <div className="content-button">
-                <Button onClickButton={onClickButton} name={nameButton} />
+                <Button onClickButton={onClickButton} name={nameButton}/>
             </div>
         </>
     )
